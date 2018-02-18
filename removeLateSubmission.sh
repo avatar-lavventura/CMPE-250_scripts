55cc929875f7ac058a924a00d2c94f9da144f9e4
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-18 16:20:48 +0300

    Update removeLateSubmission.sh

--- removeLateSubmission.sh
+++ removeLateSubmission.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-for d in deneme2*/ ; do
-    #echo "$d"
+#http://www.timestampgenerator.com/1518847200/#result
+FOLDER="deneme2"
+python gh_classroom_collect.py "CMPE483-Blockchain-Spring-2018-Bogazici" "$FOLDER" $FOLDER --user avatar-lavventura --token password.txt --due-date 1518847200
+
+cd $FOLDER/
+for d in $FOLDER*/ ; do
+    echo "$d"
     cd $d
     branchName=$(git rev-parse --abbrev-ref HEAD) #Branch name only
 
@@ -9,5 +14,5 @@ for d in deneme2*/ ; do
 	mv ../$d ../LATE
     fi
     cd ..
-    #echo '-----------------------------'
+    echo '-----------------------------'
 done
